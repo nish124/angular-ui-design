b61405175bf482c4a2d3fb2660e02d18a3876010
Nisha_ <ndhere@grepruby.io_>
2020-06-06 00:45:08 +0530

    Header part uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {MenuItem} from 'primeng/api';
+import { HeaderpartComponent } from './headerpart/headerpart.component';
+import { FooterpartComponent } from './footerpart/footerpart.component';
+import { MidpartComponent } from './midpart/midpart.component';                  //api
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderpartComponent,
+    FooterpartComponent,
+    MidpartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+   BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/midpart/midpart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-midpart',
+  templateUrl: './midpart.component.html',
+  styleUrls: ['./midpart.component.scss']
+})
+export class MidpartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
